ea6a4a53ca5b3734e0bb44f894aa966201aff40f
Andjela Milicevic <andjelabor@gmail.com>
2018-08-30 12:23:30 +0200

    Add project files.

---
+++ Rasadnik/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rasadnik.MvcApplication" Language="C#" %>
---
+++ Rasadnik/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rasadnik.Startup))]
+namespace Rasadnik
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
